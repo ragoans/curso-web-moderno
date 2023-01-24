000faaed927ca82a8d2c3eb22e4263e12acdeafc
ragoans <rafaelgoulart12@gmail.com>
2023-01-24 13:27:27 -0400

    Organizado

--- exercicios-js/fundamentos/046-array.js
+++ exercicios-js/fundamentos/046-array.js
@@ -1,23 +1,32 @@
-const valores = [7.7, 8.9, 6.3, 9.2]
-console.log(valores[0], valores[3])
 
 // printa de acordo com a posição do array sendo a primeira com o valor 0.
-
+const valores = [7.7, 8.9, 6.3, 9.2]
+console.log(valores[0], valores[3])
 console.log(valores[4]) //undefined já que não existe a posição 4
 
-valores [4] = 10 //adiciona ou troca valores no array
-console.log(valores) 
-console.log(valores.length) //número de elementos na array
 
-
-valores.push({id: 3}, false, null, 'teste') //adiciona itens no final da array
+//adiciona ou troca valores no array
+valores [4] = 10 
 console.log(valores)
+
+
+//número de elementos na array
+console.log(valores.length) 
+
+
+//adiciona itens no final da array
+valores.push({id: 3}, false, null, 'teste') 
+console.log(valores)
+
+//retorna o último valor do array
+console.log(valores.pop()) 
+
+
+//deleta valor na posição
+delete valores[0] 
+console.log(valores)
+
+//array tipo objeto
+console.log(typeof valores) 
 
 //ideal array de tipos de dados diferentes
-
-console.log(valores.pop()) //retorna o último valor do array
-
-delete valores[0] //deleta valor na posição
-console.log(valores)
-
-console.log(typeof valores) //array tipo objeto
